4e57c52836922bb9a1e8fd31e298e00c742f90a9
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-02-28 12:59:30 -0300

    Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
+<reference path="../../typings/globals/jquery/index.d.ts" />
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-toggle="popover"]').popover();
+
+    $("#btn1").click(function(){
+        alert("O botão de ciclovias da cidade foi clicado")
+        console.log("O botão foi clicado")
+    })
 })
